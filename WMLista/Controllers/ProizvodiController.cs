64852ad50bf90afd29ade63262f093babe91805a
MilanAdamovic <37418775+MilanAdamovic@users.noreply.github.com>
2020-05-24 19:39:24 +0200

    Initial commit

---
+++ WMLista/Controllers/ProizvodiController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WMLista.Models;
+
+namespace WMLista.Controllers
+{
+    public class ProizvodiController : Controller
+    {
+        // GET: Proizvodi
+        public ActionResult Index()
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                return View(dbModel.Proizvodis.ToList());
+            }
+            
+        }
+
+        // GET: Proizvodi/Details/5
+        public ActionResult Details(int id)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                return View(dbModel.Proizvodis.Where(x=>x.Id == id).FirstOrDefault());
+
+            }
+        }
+
+        // GET: Proizvodi/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Proizvodi/Create
+        [HttpPost]
+        public ActionResult Create(Proizvodi proizvodi)
+        {
+            try
+            {
+                using (DbModels dbModel = new DbModels())
+                {
+                    dbModel.Proizvodis.Add(proizvodi);
+                    dbModel.SaveChanges();
+                }
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Proizvodi/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                return View(dbModel.Proizvodis.Where(x => x.Id == id).FirstOrDefault());
+
+            }
+            
+        }
+
+        // POST: Proizvodi/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Proizvodi proizvodi)
+        {
+            try
+            {
+                using (DbModels dbModels = new DbModels())
+                {
+                    dbModels.Entry(proizvodi).State = EntityState.Modified;
+                    dbModels.SaveChanges();
+                }
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Proizvodi/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                return View(dbModel.Proizvodis.Where(x => x.Id == id).FirstOrDefault());
+
+            }
+        }
+
+        // POST: Proizvodi/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (DbModels dbModels = new DbModels())
+                {
+                    Proizvodi proizvodi = dbModels.Proizvodis.Where(x => x.Id == id).FirstOrDefault();
+                    dbModels.Proizvodis.Remove(proizvodi);
+                    dbModels.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WMLista/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WMLista.MvcApplication" Language="C#" %>
